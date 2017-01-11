03ba1687afa28ab8ae7e3834e09fd82be9bd78dc
tianye19941001 <838199892@qq.com>
2017-01-11 09:22:19 +0800

    反向

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,7 +1,7 @@
 var Article = require('../models/articles');
 
 exports.index = function(req,res){
-	Article.find({}).limit(3).exec(function(err,articles){
+	Article.find({}).sort({_id: -1}).limit(3).exec(function(err,articles){
 		res.render('index',{
 			articles:articles
 		})
